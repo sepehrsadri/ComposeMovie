12a870688d0c58f27c2ede1b8042ea2dc41347e6
sepehr.sadri <sepehrsadri@gmail.com>
2023-07-17 19:17:19 +0330

    Fix: data model json fileds

--- feature/search/src/main/java/com/sadri/composemovie/search/api/response/MovieResponseDto.kt
+++ feature/search/src/main/java/com/sadri/composemovie/search/api/response/MovieResponseDto.kt
@@ -2,22 +2,17 @@ package com.sadri.composemovie.search.api.response
 
 import com.google.gson.annotations.SerializedName
 
-internal data class MovieDataResponseDto(
+data class MovieDataResponseDto(
   @SerializedName("data") val data: List<MovieItemResponseDto>
 )
 
-internal data class MovieItemResponseDto(
-  @SerializedName("id") val id: Long,
-  @SerializedName("attributes") val attribute: MovieAttributesDto
-)
-
-internal data class MovieAttributesDto(
-  @SerializedName("id") val id: Long,
+data class MovieItemResponseDto(
+  @SerializedName("movie_id") val id: String,
   @SerializedName("movie_title") val title: String,
   @SerializedName("pic") val picture: MoviePictureDto,
   @SerializedName("descr") val description: String
 )
 
-internal data class MoviePictureDto(
+data class MoviePictureDto(
   @SerializedName("movie_img_b") val url: String
 )
