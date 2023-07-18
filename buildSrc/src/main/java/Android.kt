db2ea907b2a7d2546ff16396b5848934fc52b4e5
sepehr.sadri <sepehrsadri@gmail.com>
2023-07-18 20:45:07 +0330

    Build: bump version to 2.0

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -4,8 +4,8 @@ object Android {
     const val targetSdkVersion = 33
     const val compileSdkVersion = 33
     const val applicationId = "com.sadri.composeMovie"
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "2.0"
     const val debug = "debug"
     const val release = "release"
 }
