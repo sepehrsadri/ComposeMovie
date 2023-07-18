72a4072a4077b5aff7ea8c5357dfbea5b64bde19
sepehr.sadri <sepehrsadri@gmail.com>
2023-07-18 18:54:27 +0330

    Refactor: separate use case logics to domain module

--- library/domain/src/main/java/com/sadri/composemovie/domain/MovieUseCase.kt
+++ library/domain/src/main/java/com/sadri/composemovie/domain/MovieUseCase.kt
@@ -1,9 +1,10 @@
 package com.sadri.composemovie.domain
 
+import com.sadri.composemovie.data.respository.MovieRepository
 import javax.inject.Inject
 
-class SearchMovieUseCase @Inject constructor(
-  private val searchRepository: com.sadri.composemovie.data.respository.MovieRepository
+class MovieUseCase @Inject constructor(
+  private val movieRepository: MovieRepository
 ) {
-  suspend operator fun invoke(query: String) = searchRepository.search(query)
+  suspend operator fun invoke(query: String) = movieRepository.search(query)
 }
